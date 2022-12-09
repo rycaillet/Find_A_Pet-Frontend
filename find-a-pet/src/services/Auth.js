85a4c52090182ddd964a1f2f56e73047c108af1d
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-12-09 11:03:11 -0500

    added registerUser to auth.js

--- find-a-pet/src/services/Auth.js
+++ find-a-pet/src/services/Auth.js
@@ -1,1 +1,10 @@
+import Client from './api'
 
+export const RegisterUser = async (data) => {
+  try {
+    const res = await Client.post('/register', data)
+    return res.data
+  } catch (error) {
+    throw error
+  }
+}
