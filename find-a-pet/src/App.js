e4fdf465624ff6a579ba75be22e28d068cb4d52a
Ryan Caillet <Ryancaillet1995@gmail.com>
2022-12-09 10:15:11 -0500

    added skeleton for app.js

--- find-a-pet/src/App.js
+++ find-a-pet/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css'
+import { Routes, Route } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { CheckSession } from './services/Auth'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    localStorage.clear()
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <header></header>
+        <main>
+          <Routes></Routes>
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
